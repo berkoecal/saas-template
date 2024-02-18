9ff899fbebe667df110850f70af3f5ae3b91d6d4
Berk <berkoecal@googlemail.com>
2024-02-18 10:25:01 +0100

    first test

--- back-end/resources/pulumi/variables.ts
+++ back-end/resources/pulumi/variables.ts
@@ -3,4 +3,5 @@ import { Output } from "@pulumi/pulumi";
 export const variables = {
   region: "eu-central-1" as const, // <-- change this to your region
   dynamoDBTables: {} as Record<string, Output<string>>,
+  //test
 };
