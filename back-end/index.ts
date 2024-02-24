08cfb03790742ab48e5f9fe421a0d5b448bea671
Berk <berkoecal@googlemail.com>
2024-02-24 21:19:27 +0100

    sinnloser change

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,6 +1,6 @@
-export { dynamoID } from './resources/pulumi/dynamodb';
-export { appSyncID, graphQLEndpoint } from './resources/pulumi/appSync';
-export { userpoolID, userpoolClientID, identityPoolID } from './resources/pulumi/userpool';
+export { dynamoID } from "./resources/pulumi/dynamodb";
+export { appSyncID, graphQLEndpoint } from "./resources/pulumi/appSync";
+export { userpoolID, userpoolClientID, identityPoolID } from "./resources/pulumi/userpool";
 
 /**
  * This is the entry file for Pulumi to start.
